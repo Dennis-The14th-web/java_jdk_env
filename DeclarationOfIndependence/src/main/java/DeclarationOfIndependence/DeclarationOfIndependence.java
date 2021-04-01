19214e39fe41aaebf05b9d0321db7e3ade98c102
louis mane <dennis.itua1@gmail.com>
2021-03-31 21:20:23 -0400

    updating the use of 2 dimensional array

--- DeclarationOfIndependence/src/main/java/DeclarationOfIndependence/DeclarationOfIndependence.java
+++ DeclarationOfIndependence/src/main/java/DeclarationOfIndependence/DeclarationOfIndependence.java
@@ -1,65 +1,93 @@
 package DeclarationOfIndependence.src.main.java.DeclarationOfIndependence;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 import java.io.File;
 //import java.util.Arrays;
 //import java.util.List;
+// import java.util.Scanner;
 /**
  *
  * @author dennisthe14th
  */
 public class DeclarationOfIndependence {
-    public static void main(String[] args) throws FileNotFoundException 
-    {   
-        //Set the delimiter used in file
-        try ( //Get scanner instance
-                Scanner scanner = new Scanner(new File("DeclarationOfIndependence/src/Files/decFile.csv"))) {
+        public static void main(String[] args)  throws FileNotFoundException 
+        {   
             //Set the delimiter used in file
-            scanner.useDelimiter(",");
+            try ( //Get scanner instance
+                    Scanner scanner = new Scanner(new File("DeclarationOfIndependence/src/Files/decFile.csv"))) {
+                //Set the delimiter used in file
+                scanner.useDelimiter(",");
+                
+                //Get input string of the scanner object
+                String string = scanner.nextLine();
+                
+                //Defined count
+                int count;
+                
+                //Converts the string into lowercase
+                string = string.toLowerCase();
+
             
-            //Get input string of the scanner object
-            String string = scanner.nextLine();
+                //Split the string into words using built-in function
             
-            //Defined count
-            int count;
-            
-            //Converts the string into lowercase
-            string = string.toLowerCase();
 
-          
-            //Split the string into words using built-in function
-           
+                // TODO
+                // Remove the replace method and use the remove method
+                // will have to convert scanner to a string or apply any other available 
+                //excluding these words "of", "the", "to", "and","for"
+                String replaceString = string.replace("of", "")
+                        .replace("the", "").replace("to", "")
+                        .replace("and", "").replace("for", "");
 
-            // TODO
-            // Remove the replace method and use the remove method
-            // will have to convert scanner to a string or apply any other available 
-            //excluding these words "of", "the", "to", "and","for"
-            String replaceString = string.replace("of", "")
-                      .replace("the", "").replace("to", "")
-                     .replace("and", "").replace("for", "");
+                String words[] = replaceString.split("([,-.\\s]+)");
 
-             String words[] = replaceString.split("([,-.\\s]+)");
+            System.out.println("100 MOST COMMON WORDS IN THE DECLARATION OF INDEPENDENCE: ");
+            for(int i = 0; i < words.length; i++) {
+                count = 1;
+                for(int j = i + 1; j < words.length; j++) {
+                    if(words[i].equals(words[j])) {
+                        count++;
+                        //Set words [j] to 0 to avoid visited word
+                        words[j] = "0";
+                    }
+                
+            }        
 
-        System.out.println("100 MOST COMMON WORDS IN THE DECLARATION OF INDEPENDENCE: ");
-        for(int i = 0; i < words.length; i++) {
-            count = 1;
-            for(int j = i + 1; j < words.length; j++) {
-                if(words[i].equals(words[j])) {
-                    count++;
-                    //Set words [j] to 0 to avoid visited word
-                    words[j] = "0";
-                }
-            
-         }        
-
-            //Display the duplicated word if counts is greate than 1
-              if(count > 1 && words[i] != "0")
-                System.out.println(count + " " + "," + " " + words[i]);
-              
+                //Display the duplicated word if counts is greate than 1
+                if(count > 1 && words[i] != "0")
+                    System.out.println(count + " " + "," + " " + words[i]);
+                
+            }
+            scanner.close(); 
         }
-         scanner.close(); 
+
+        /*
+        Let's build a chessboard with a 2 diminsional array
+        java gives us the opportunity of creating a 2 dimensional array
+        How sweet, that is why we are use a nested for loop to interact with these arrays.
+        */
+
+        // int boardDim = 8;
+        // char[][] board = new char[boardDim][boardDim];
+        // boolean isWhite = false;
+
+        // for(int y = 0; y < board.length; y++) 
+        // {
+        //     isWhite = !isWhite;
+
+        //     for(int x = 0; x < board[y].length; x++) 
+        //     {
+        //         if(isWhite) board[y][x] = 'W';
+        //         if(!isWhite) board[y][x] = 'B';
+        //         isWhite = !isWhite;
+        //     }
+        // }
+
+        // for(int i = 0; i < board.length; i++)
+        // {
+        //    System.out.println(Arrays.toString(board[i]));
+        // }
     }
-}
 
 }
 
