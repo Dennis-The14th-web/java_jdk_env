77b2946fa3309ddc7c6b498c58bf238e73c4f001
louis mane <dennis.itua1@gmail.com>
2021-04-02 00:56:31 -0400

    updated Chessboard game

--- DeclarationOfIndependence/src/main/java/DeclarationOfIndependence/DeclarationOfIndependence.java
+++ DeclarationOfIndependence/src/main/java/DeclarationOfIndependence/DeclarationOfIndependence.java
@@ -60,33 +60,6 @@ public class DeclarationOfIndependence {
             }
             scanner.close(); 
         }
-
-        /*
-        Let's build a chessboard with a 2 diminsional array
-        java gives us the opportunity of creating a multi-deminsional array
-        How sweet, that is why we are using a nested for loop to iterate on these arrays.
-        */
-
-        // int boardDim = 8;
-        // char[][] board = new char[boardDim][boardDim];
-        // boolean isWhite = false;
-
-        // for(int y = 0; y < board.length; y++) 
-        // {
-        //     isWhite = !isWhite;
-
-        //     for(int x = 0; x < board[y].length; x++) 
-        //     {
-        //         if(isWhite) board[y][x] = 'W';
-        //         if(!isWhite) board[y][x] = 'B';
-        //         isWhite = !isWhite;
-        //     }
-        // }
-
-        // for(int i = 0; i < board.length; i++)
-        // {
-        //    System.out.println(Arrays.toString(board[i]));
-        // }
     }
 
 }
---
+++ DeclarationOfIndependence/src/main/java/songs.java
@@ -0,0 +1,11 @@
+package DeclarationOfIndependence.src.main.java;
+
+import java.util.*;
+
+public class songs {
+    public static void main (String[] args){
+         
+         }
+    }
+    
+}
