b583f4900a37f5f3f83d5ef63b16029e7ecc3382
louis mane <dennis.itua1@gmail.com>
2021-04-13 04:29:05 -0400

    updated JavaFunctions2

---
+++ DeclarationOfIndependence/src/main/java/DeclarationOfIndependence/JavaFuctions2.java
@@ -0,0 +1,31 @@
+package DeclarationOfIndependence;
+import java.util.*;
+
+public class JavaFuctions2 {
+    public static void main(String[] args) 
+    {
+            /*
+            This scanner version prompts users to insert input
+            */
+
+            Scanner reader = new Scanner(System.in);
+            String inputType;
+
+            System.out.print("Input type (word or sentense): ");
+            inputType = reader.next();
+            System.out.println("Reversed  string output: " + JavaFuctions2.reverseString(inputType));
+            System.out.println("Closing scanner...");
+            reader.close();
+            System.out.println("Scanner closed");
+    }
+
+    public static String reverseString(String str)
+    {
+    //        StringBuilder strVal = new StringBuilder(str);
+    //        strVal.reverse();
+    //        return strVal.toString();
+    return new StringBuilder(str).reverse().toString();
+    }
+}
+
+
--- DeclarationOfIndependence/src/main/java/DeclarationOfIndependence/src/main/java/JavaFunctions.java
+++ DeclarationOfIndependence/src/main/java/DeclarationOfIndependence/src/main/java/JavaFunctions.java
@@ -48,26 +48,3 @@ public class JavaFunctions {
         return result ;
     }
 }
-
-/*
-This scanner version prompts users to insert input
-*/
-
-// Scanner reader = new Scanner(System.in);
-// String inputType;
-
-// System.out.print("Input type (word or sentense): ");
-// inputType = reader.next();
-// System.out.println("Reversed  string output: " + JavaFunctions.reverseString(inputType));
-// System.out.println("Closing scanner...");
-// reader.close();
-// System.out.println("Scanner closed");
-// }
-
-// public static String reverseString(String str)
-// {
-// //        StringBuilder strVal = new StringBuilder(str);
-// //        strVal.reverse();
-// //        return strVal.toString();
-// return new StringBuilder(str).reverse().toString();
-// }
